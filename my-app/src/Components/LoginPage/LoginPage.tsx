db3d327f059b4c6cb279c3a079f4f1504cae4cb1
LoganJanes <lmjanes@mun.ca>
2024-09-30 12:59:29 -0230

    Set up react framework
    Created the react framework and set up the login page template

---
+++ my-app/src/Components/LoginPage/LoginPage.tsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import React from "react";
+import './LoginPage.css'
+import Lightbulb from '../Assets/Lightbulb.png'
+import Logo from '../Assets/Logo.png'
+import People from '../Assets/People.png'
+
+const LoginPage = () => {
+return (
+    <div className = 'container'>
+        <div className = 'header'>
+        <div className="text">Login </div>
+      
+    </div>
+    </div>
+
+);
+
+};
+
+
+export default LoginPage;
