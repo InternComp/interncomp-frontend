7516b2485008822c54de8bd8a17c1f955dd6c8ca
LoganJanes <lmjanes@mun.ca>
2024-09-30 13:30:45 -0230

    Adding Images
    Adding image code so they can be added to Login Page

--- my-app/src/Components/LoginPage/LoginPage.tsx
+++ my-app/src/Components/LoginPage/LoginPage.tsx
@@ -1,13 +1,16 @@
 import { useState } from "react";
 import React from "react";
 import './LoginPage.css'
-import Lightbulb from '../Assets/Lightbulb.png'
-import Logo from '../Assets/Logo.png'
-import People from '../Assets/People.png'
+import Lightbulb from '../Assets/Icons/Lightbulb.png'
+import Logo from '../Assets/Icons/Logo.png'
+import People from '../Assets/Icons/People.png'
 
 const LoginPage = () => {
 return (
     <div className = 'container'>
+        <header> 
+            <img src = {Logo} alt='' />
+        </header>
         <div className = 'header'>
         <div className="text">Login </div>
       
