24513cc873a027e76cc2bbd9b9b52e7764c8a8d6
LoganJanes <lmjanes@mun.ca>
2024-10-02 15:11:09 -0230

    Added company page.

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import LoginPage from './Components/LoginPage/LoginPage'
+import CompanyPage from './Components/CompanyPage/CompanyPage'
 
 function App() {
   return (
     <div>
-     <LoginPage/>
+     <CompanyPage/>
     </div>
   );
 }
