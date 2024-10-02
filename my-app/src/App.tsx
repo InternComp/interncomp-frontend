fe411befde37ff683051cb4c8eb519e2fe291bab
LoganJanes <lmjanes@mun.ca>
2024-10-02 15:00:59 -0230

    fixing App.cs info
    Fixing info for App.cs

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import LoginPage from './Components/LoginPage/LoginPage'
+import  LoginPage from './Components/LoginPage/LoginPage'
 
 function App() {
   return (
