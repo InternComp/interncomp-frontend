2704d6b2eadf887ebfe4e1b401b288d565b9f149
aryasalwan <aryas@mun.ca>
2024-10-12 20:13:53 -0230

    integrated the two branches

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,27 @@
-import React from "react";
+import React, { useState } from 'react';
 import Header from "./Components/Header/Header";
 import Footer from "./Components/Footer/Footer";
 import bestIndustry from "./assets/bestIndustry.png";
-
+import LoginPage from "./Components/LoginPage/LoginPage";
+import Home from "./Components/Home/Home"
 const App = () => {
+  const [activeComponent, setActiveComponent] = useState('Home');
+
+  const renderComponent = () => {
+    switch (activeComponent) {
+      case 'LoginPage':
+        return <LoginPage />;
+        case 'Home':
+          return <Home />;
+    }
+  };
+
   return (
     <>
       <div className="w-[95%] m-auto bg-white">
-        <Header />
+        <Header setActiveComponent={setActiveComponent} />
       </div>
-
-      <div className="w-full">
-        <img src={bestIndustry} alt="Join the best companies" className="w-full max-w-[100%] " />
-      </div>
-
+      {renderComponent()}
       <div className="w-[95%] m-auto bg-white">
         <Footer />
       </div>
